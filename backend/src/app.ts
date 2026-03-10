f2950ea7f3481c889f9ccc47d1b2693eccb5e203
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-03-10 00:50:22 -0300

    ✨ feat: Add prisma integration ORM

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,17 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes";
+import { PrismaClient } from "@prisma/client";
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: 'Henrry',
+    email: 'Henrrylimadasilva@gmail.com',
+  }
+});
 
 app.register(appRoutes, {
   prefix: 'app',
