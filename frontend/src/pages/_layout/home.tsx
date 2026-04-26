87558a1df6d6e26de9645ff8a54556fe0971b723
José Caique <caique.alves02@gmail.com>
2026-04-26 02:23:47 -0300

    feat: home header responsive and routing

--- frontend/src/pages/_layout/home.tsx
+++ frontend/src/pages/_layout/home.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import { HomeHeader } from "@/components/home/home-header";
 
 export function HomeLayout() {
   return (
     <div className="flex flex-col min-h-screen antialiased">
-      <Outlet />
+      <HomeHeader />
+      <div className="flex-1">
+        <Outlet />
+      </div>
     </div>
   );
 }
